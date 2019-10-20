16b56fd09df64d998cf10bca6d632719da9753b4
Aquila Payton <44341215+Apayton1@users.noreply.github.com>
2019-10-20 15:08:30 -0400

    correct code

--- googlebooks/server.js
+++ googlebooks/server.js
@@ -10,23 +10,19 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 
-// let Books = require("./client/src/Models/Book");
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Define middleware here
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
--- googlebooks/src/index.js
+++ googlebooks/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import App from './App';
 // import { ProductProvider } from "./context";
@@ -8,9 +7,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     
-    <Router>
+    
         <App />
-    </Router>
+    
     
     , document.getElementById('root'));
 
